69780849ff7e55df2f6648026a32bd084efce0ff
Bewtaich <bewtaich@gmail.com>
2024-09-03 00:52:45 -0500

    prompt function

--- server.js
+++ server.js
@@ -17,34 +17,67 @@ const pool = new Pool(
     password: 'Troll571',
     host: 'localhost',
     //PostgreSQL DB
-    database: 'books_db'
+    database: 'employees_db'
   },
   console.log(`Connected to the database.`)
 )
 
 pool.connect();
 
-// const questions = {
-//   {
-//     type: "list",
-//     name: "actions",
-//     message: "What would you like to do?",
-//     choices: [],
-//   },
-// }
+const questions = [
+    {type: "list",
+    name: "actions",
+    message: "What would you like to do?",
+    choices: ['View Departments', 'View Roles', 'View Employees',
+      'Add Department', 'Add Role', 'Add Employee', "Update Employee's Role"
+    ]},
+]
 
 //View all departments
-    //Format Table
-    //Department Names
-    //Department Ids
+function viewDepartments () {
 
+}
 
 //View all roles
+function viewRoles () {
+  
+}
+
 //View all employees
+function viewEmployees () {
+  
+}
+
 //Add a role
+function addRole () {
+  
+}
+
 //Add a department
-//Ad an employee
+function addDepartment () {
+  
+}
+
+//Add an employee
+function addEmployee () {
+  
+}
+
 //Update an employee role
+function updateEmployee () {
+
+}
+
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) =>
+      console.log('Hi')
+    )};
+
+
+init();
 
 app.use((req, res) => {
   res.status(404).end();
